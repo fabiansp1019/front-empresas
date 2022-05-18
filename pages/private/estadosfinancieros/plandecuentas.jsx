0af1fb0adf99e5382be8baab960c18ba9df0e45a
prii123 <83259348+prii123@users.noreply.github.com>
2022-05-17 23:34:26 -0500

    se deja casi listo la interfaz de plan de cuentas

--- pages/private/estadosfinancieros/plandecuentas.jsx
+++ pages/private/estadosfinancieros/plandecuentas.jsx
@@ -3,11 +3,53 @@ import axios from "axios";
 import Link from "next/link";
 import LayoutPrivate from "../../../components/Layoutprivate";
 
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Typography from '@mui/material/Typography';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export const ClasePucStyle = ({ codigo, nombre }) => {
+  return (
+    <>
+      <Box
+        sx={{ width: "59vw" }} //p: 2, border: "1px dashed grey",
+      >
+        <Item>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <Typography align="left" variant="h6">{codigo}</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <Typography align="left" variant="h6">{nombre}</Typography>
+            </Grid>
+          </Grid>
+        </Item>
+      </Box>
+    </>
+  );
+};
+
+export const ClassTest = () => {
+  return (
+    
+    <Box sx={{ width:'700px', height:'300px', bgcolor:'red'}}>
+      brayan xx
+    </Box>
+    
+  )
+}
+
 const plandecuentas = () => {
   const [estadosFinancieros, setEstadosFinancieros] = useState([]);
-  const [dataa, setDataa] = useState([]);
-  const [saldosPorGrupos, setSaldosPorGrupos] = useState([]);
-  const [saldosPorCuentas, setSaldosPorCuentas] = useState([]);
 
   const getdata = async () => {
     const req = await axios({
@@ -18,8 +60,6 @@ const plandecuentas = () => {
       },
     });
     setEstadosFinancieros(req.data.lista);
-    // setDataa(req.data.auxiliares);
-    // console.log(req.data.auxiliares);
   };
 
   useEffect(() => {
@@ -27,78 +67,69 @@ const plandecuentas = () => {
     // console.log(saldos());
   }, []);
 
-  // if(dataa){
-  //   const valor = dataa.find({clase: "1"}).clase;
-  //   console.log(valor);
-  // }
-
-  // console.log(estadosFinancieros.filter((e) => e.clase == "1"));
-
   return (
     <LayoutPrivate>
       <div>
-        <div>
+      {/* <ClassTest/> */}
+      <Box sx={{with:'100vw'}}>
+      <>
           <Link href={"/private/estadosfinancieros/agregargrupos"}>
             <a>agregar grupos</a>
           </Link>
           <Link href={"/private/estadosfinancieros/plandecuentas"}>
             <a>Plan de Cuentas</a>
           </Link>
-        </div>
+        </>
 
-        <div>
           <ul>
             {estadosFinancieros.map((estado) => {
               return (
                 <>
-                  <li>
-                    {" "}
-                    Clase : {estado.clase} - {estado.nombre}
-                    {/*  console.log(dataa.find({clase: '5'}))  */}
-                  </li>
-                  <ul>
+                  <ClasePucStyle codigo={estado.clase} nombre={estado.nombre} />
+                  <>
                     {estado.grupos.map((grup) => {
                       return (
                         <>
-                          <li>
-                            grupo: {grup.grupo} - {grup.nombre}
-                          </li>
-                          <ul>
+                          <ClasePucStyle
+                            codigo={grup.grupo}
+                            nombre={grup.nombre}
+                          />
+
+                          <>
                             {grup.cuentas.map((cuenta) => {
                               return (
                                 <>
-                                  <li>
-                                    cuenta: {cuenta.cuentas} - {cuenta.nombre}
-                                  </li>
-                                  {/* <ul>
-                                              {cuenta.subcuentas.map(
-                                                (subcuenta) => {
-                                                  return (
-                                                    <>
-                                                      <li>
-                                                        sub Cuenta:{" "}
-                                                        {subcuenta.subcuentas} -{" "}
-                                                        {subcuenta.nombre}
-                                                      </li>
-                                                    </>
-                                                  );
-                                                }
-                                              )}
-                                            </ul> */}
+                                  <ClasePucStyle
+                                    codigo={cuenta.cuentas}
+                                    nombre={cuenta.nombre}
+                                  />
+                                  <>
+                                    {cuenta.subcuentas.map((subcuenta) => {
+                                      return (
+                                        <>
+                                          <ClasePucStyle
+                                            codigo={subcuenta.subcuentas}
+                                            nombre={subcuenta.nombre}
+                                          />
+                                        </>
+                                      );
+                                    })}
+                                  </>
                                 </>
                                 // aquii
                               );
                             })}
-                          </ul>
+                          </>
                         </>
                       );
                     })}
-                  </ul>
+                  </>
                 </>
               );
             })}
           </ul>
-        </div>
+      </Box>
+
       </div>
     </LayoutPrivate>
   );
